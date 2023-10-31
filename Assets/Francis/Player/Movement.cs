cfe4d0322be714627143069576a8aec0d89520f6
Francisco Anticona <francisco.anticona81@gmail.com>
2023-10-31 17:37:25 -0500

    ANTICONA Player y Tags

---
+++ Assets/Francis/Player/Movement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody2D rb2D;
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb2D.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+}
